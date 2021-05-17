8d13fbaba1c0c11e86c8c661386ced75868e96ed
Ayo Abimbola <ayoinspired@gmail.com>
2021-05-17 03:51:32 +0100

    implementing dynamically adding channels and switching names

--- src/Chat.js
+++ src/Chat.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Chat.css";
 import { useParams } from "react-router-dom";
+import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import db from "./firebase";
 
 function Chat() {
 	const { roomId } = useParams();
+	const [roomDetails, setRoomDetails] = useState(null);
+
+	useEffect(() => {
+		if (roomId) {
+			db.collection("rooms")
+				.doc(roomId)
+				.onSnapshot((snapshot) => setRoomDetails(snapshot.data()));
+		}
+	}, [roomId]);
+
 	return (
 		<div className="chat">
-			<h2>You are in the {roomId} room </h2>
+			<div className="chat__header">
+				<div className="chat__headerLeft">
+					<h4 className="chat__channelName">
+						<strong># {roomDetails?.name} </strong>
+						<StarBorderOutlinedIcon className="mui__icon" />
+					</h4>
+				</div>
+
+				<div className="chat__headerRight">
+					<p>
+						<InfoOutlinedIcon className="mui__icon" /> Details
+					</p>
+				</div>
+			</div>
 		</div>
 	);
 }
