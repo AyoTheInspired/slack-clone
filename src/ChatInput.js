813dfde10ce832b4fab2dc7f358badeacc79d297
Ayo <bimboladxtreme@gmail.com>
2021-05-19 04:10:49 +0100

    finalizing chat interface.

---
+++ src/ChatInput.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import db from './firebase'
+import './ChatInput.css'
+import { useStateValue } from './StateProvider'
+import firebase from 'firebase'
+
+function ChatInput({channelName, channelId }) {
+
+const [input, setInput] = useState("")
+const [{ user }] = useStateValue()
+
+const sendMessage = (e) => {
+ e.preventDefault();
+ 
+ console.log(e)
+
+ if(e.target.value) {
+  console.log('input has a value')
+ }else {
+  console.log('input has no value')
+ }
+
+
+
+if (channelId) {
+ db.collection('rooms').doc(channelId).collection('messages').add({
+  message: input,
+  timestamp: firebase.firestore.FieldValue.serverTimeStamp(),
+  user: user.displayName,
+  userImage: user.photoURL,
+ })
+}
+}
+
+
+ return (
+  
+  <div className="chatInput">
+  <form onSubmit={sendMessage}>
+  <input
+  type="text"
+  value={input}
+  onInput={(e) => setInput(e.target.value)}
+  placeholder={`Message Here...`} 
+  />
+   <button type="submit" onClick={sendMessage}>Send</button>
+  </form>
+  </div>
+
+ )
+
+}
+
+export default ChatInput;
--- src/Header.js
+++ src/Header.js
@@ -4,16 +4,20 @@ import { Avatar } from "@material-ui/core";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
 import SearchIcon from "@material-ui/icons/Search";
 import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
+import {useStateValue} from './StateProvider'
 
 function Header() {
+const [{user}] = useStateValue()
+
+
 	return (
 		<div className="header">
 			<div className="header__left">
 				<Avatar
 					className="header__avatar"
-					alt="The Inspired"
-					// src={user?.photoURL}
-					src=""
+					src={user?.photoURL}
+					alt={user?.photoURL}
+				
 				/>
 				<AccessTimeIcon />
 
